a80599972d4f249b07bf84bb78cbdc63d036313b
SeiichiN <billie175@gmail.com>
2020-08-22 22:34:30 +0900

    少し名称を整理した。

--- src/helper/Board.java
+++ src/helper/Board.java
@@ -8,15 +8,15 @@ import model.MasuData;
 public class Board {
     private int row = 6;
     private int col = 6;
-    private ArrayList<MasuData> board;
+    private ArrayList<MasuData> arrayMasuData;
 
     public Board () {
-        board = new ArrayList <MasuData> ();
+        arrayMasuData = new ArrayList <MasuData> ();
         init();
     }
     
     public Board (int row, int col) {
-    	board = new ArrayList <MasuData> ();
+    	arrayMasuData = new ArrayList <MasuData> ();
     	this.row = row;
     	this.col = col;
     	init();
@@ -40,23 +40,23 @@ public class Board {
                 MasuData masu = new MasuData( i, t );
                 // System.out.println( "Board:41:count:" + count );
                 masu.setNum( count );
-                board.add( masu );
+                arrayMasuData.add( masu );
                 count++;
             }
         }
     }
 
-    public ArrayList<MasuData> getBoard () { return this.board; }
-    public void setBoard( ArrayList<MasuData> board ) {
-        this.board = board;
+    public ArrayList<MasuData> getBoard () { return this.arrayMasuData; }
+    public void setBoard( ArrayList<MasuData> _arrayMasuData ) {
+        this.arrayMasuData = _arrayMasuData;
     }
 
     public MasuData get (int i) {
-        return board.get( i );
+        return arrayMasuData.get( i );
     }
 
     public void add (MasuData masuData) {
-    	board.add( masuData );
+    	arrayMasuData.add( masuData );
     }
 
     /**
@@ -67,10 +67,10 @@ public class Board {
      *   0...35
      */
     public int getIndex( int row, int col ) {
-//        if (row < 1 || row > row) {
+//        if (row < 1 || row > this.row) {
 //            System.out.println("範囲外");
 //            return 0;
-//        } else if (col < 1 || col > col) {
+//        } else if (col < 1 || col > this.col) {
 //            System.out.println("範囲外");
 //            return 0;
 //        }
@@ -78,9 +78,10 @@ public class Board {
     }
 
     /**
-     *
-     * @param direction -- 方向 0...7 0:北 1:北東 2:東
-     * @param nowMasu     -- 現在地のマス
+     * 現在地(nowMasu)を基準として、direction の方向にある隣点の
+     * 情報を得る
+     * @param int direction -- 方向 0...7 0:北 1:北東 2:東
+     * @param MasuData nowMasu     -- 現在地のマス
      * @return MasuData masuData -- その地点のマスデータ
      */
     public MasuData neighbor (int direction, MasuData nowMasu) {
@@ -109,54 +110,66 @@ public class Board {
      *   6 | * | 2
      *  ---+---+---
      *   5 | 4 | 3
+     *   
+     *   隣の地点は全部で8個ある。もしその地点が盤外なら
+     *   Color.RED とする。
+     *   盤外でなかったら、その地点の情報をセットする。
      */
     public MasuData[] neighbors (int row, int col) {
         MasuData[] masus = new MasuData[8];
 
+        // 0
         if (isOnBoard(row - 1 , col)) {
-            masus[0]= board.get( getIndex( row - 1, col ) );
+            masus[0]= arrayMasuData.get( getIndex( row - 1, col ) );
         } else {
             masus[0] = new MasuData( Color.RED );
         }
 
+        // 1
         if (isOnBoard(row - 1 , col + 1)) {
-            masus[1]= board.get( getIndex( row - 1, col + 1) );
+            masus[1]= arrayMasuData.get( getIndex( row - 1, col + 1) );
         } else {
             masus[1] = new MasuData( Color.RED );
         }
 
+        // 2
         if (isOnBoard(row , col + 1)) {
-            masus[2]= board.get( getIndex( row, col + 1) );
+            masus[2]= arrayMasuData.get( getIndex( row, col + 1) );
         } else {
             masus[2] = new MasuData( Color.RED );
         }
 
+        // 3
         if (isOnBoard(row + 1, col + 1)) {
-            masus[3]= board.get( getIndex( row + 1, col + 1) );
+            masus[3]= arrayMasuData.get( getIndex( row + 1, col + 1) );
         } else {
             masus[3] = new MasuData( Color.RED );
         }
 
+        // 4
         if (isOnBoard(row + 1, col)) {
-            masus[4]= board.get( getIndex( row + 1, col) );
+            masus[4]= arrayMasuData.get( getIndex( row + 1, col) );
         } else {
             masus[4] = new MasuData( Color.RED );
         }
 
+        // 5
         if (isOnBoard(row + 1, col - 1)) {
-            masus[5]= board.get( getIndex( row + 1, col - 1) );
+            masus[5]= arrayMasuData.get( getIndex( row + 1, col - 1) );
         } else {
             masus[5] = new MasuData( Color.RED );
         }
 
+        // 6
         if (isOnBoard(row, col - 1)) {
-            masus[6]= board.get( getIndex( row, col - 1) );
+            masus[6]= arrayMasuData.get( getIndex( row, col - 1) );
         } else {
             masus[6] = new MasuData( Color.RED );
         }
 
+        // 7
         if (isOnBoard(row - 1, col - 1)) {
-            masus[7]= board.get( getIndex( row - 1, col - 1) );
+            masus[7]= arrayMasuData.get( getIndex( row - 1, col - 1) );
         } else {
             masus[7] = new MasuData( Color.RED );
         }
